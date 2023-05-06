9755b4c15dc24dab5d9d740d8edffd9b5633e3e2
“dashingaryan07” <rajaryanpanipat@gmail.com>
2023-05-06 23:03:57 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Create from "./components/Create"
+import Read from "./components/Read"
+import Update from "./components/Update"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Create />}/>
+          <Route path='/read' element={<Read />} />
+          <Route path='/:id' element={<Update />} />
+        </Routes>
+        </BrowserRouter>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Read.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
+const Read = () => {
+
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  async function getData() {
+    const response = await fetch("http://localhost:5000");
+    const result = await response.json();
+    console.log("result..", result);
+    if (!response.ok) {
+      setError(result.error);
+    }
+    if (response.ok) {
+      console.log(response.ok);
+      setData(result);
+      setError("");
+    }
+  }
+
+const handleDelete = async (id) => {
+    const response = await fetch(`http://localhost:5000/${id}`, {
+      method: "DELETE"
+    });
+    const result = await response.json();
+    if (!response.ok) {
+      console.log(result.error);
+      setError(result.error);
+    }
+    if (response.ok) {
+      console.log(response.ok);
+      setError("Deleted Successfully");
+
+    setTimeout(() => {
+      setError("");
+      getData();
+    }, 5000);
+  }
+}
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className="container my-2">
+      {error && <div className="alert alert-danger"> {error} </div>}
+      <div className="row">
+        {data?.map((ele) => (
+          <div key={ele._id} className="col-3">
+            <div className="card my-2 ">
+              <div className="card-body">
+                <h5 className="card-title">{ele.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{ele.email}</h6>
+                <p className="card-text">{ele.age}</p>
+                <a onClick={() => handleDelete(ele._id)} href="" className="card-link">Delete</a>
+                <Link to={`/${ele._id}`} className="card-link">Edit</Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Read
